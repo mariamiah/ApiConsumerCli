c1eb556a586cf0b8e1f20ea061758db36a5400a7
= <=>
2018-09-20 19:25:13 +0300

    Modify setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='apiconsumercli',
     version='1.0',
     py_modules=['api'],
-    install_requires=['click'],
+    install_requires=['click', 'requests'],
     entry_points='''[console_scripts]
                      api = api:cli ''',
 )
