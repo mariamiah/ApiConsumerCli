5be6d0c867f5c19d32aff03b4bdd18b8391ac82e
= <=>
2018-09-20 19:30:39 +0300

    Modify api.py file

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ import requests
 def cli():
     source_request = requests.get("https://newsapi.org/v2/sources?apiKey=a117f0a67f6146999368f24be6ab37f3")
     source_request_dict = source_request.json()
-    source_names = source_request_dict['sources']
-    for source in source_names:
+    sources = source_request_dict['sources']
+    for source in sources:
         click.echo(source['name'])
 
 
